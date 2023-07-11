0d642fbfb30dc37985550e77a9c0eb1d3b047816
= <=>
2023-07-10 21:54:03 -0300

    D.1.2

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -15,22 +15,27 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+
+    private Double score;
     private String imgUrl;
-    private String shortDecription;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
 
     }
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDecription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
-        this.shortDecription = shortDecription;
+        this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
 
@@ -66,12 +71,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -82,12 +95,12 @@ public class Game {
         this.imgUrl = imgUrl;
     }
 
-    public String getShortDecription() {
-        return shortDecription;
+    public String getShortDescription() {
+        return shortDescription;
     }
 
-    public void setShortDecription(String shortDecription) {
-        this.shortDecription = shortDecription;
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
     }
 
     public String getLongDescription() {
